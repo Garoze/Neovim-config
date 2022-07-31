888b19076db8418c391620c7c212434b2392786d
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-07-30 23:50:45 -0300

    refactore: Changed the options.lua format

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,39 +1,45 @@
-vim.opt.backup = false                          
-vim.opt.clipboard = "unnamedplus"               
-vim.opt.cmdheight = 2                           
-vim.opt.completeopt = { "menuone", "noselect" } 
-vim.opt.conceallevel = 0                        
-vim.opt.fileencoding = "utf-8"                  
-vim.opt.hlsearch = false 
-vim.opt.ignorecase = true                       
-vim.opt.mouse = "a"                             
-vim.opt.pumheight = 10                          
-vim.opt.showmode = false                        
-vim.opt.showtabline = 2                         
-vim.opt.smartcase = true                        
-vim.opt.smartindent = true                      
-vim.opt.splitbelow = true                       
-vim.opt.splitright = true                       
-vim.opt.swapfile = false                        
-vim.opt.termguicolors = true                    
-vim.opt.timeoutlen = 1000                       
-vim.opt.undofile = true                         
-vim.opt.updatetime = 300                        
-vim.opt.writebackup = false                     
-vim.opt.expandtab = true                        
-vim.opt.shiftwidth = 2                          
-vim.opt.tabstop = 2                             
-vim.opt.cursorline = true                       
-vim.opt.number = true                           
-vim.opt.relativenumber = true                   
-vim.opt.numberwidth = 4                         
-vim.opt.signcolumn = "yes"                      
-vim.opt.wrap = false                            
-vim.opt.scrolloff = 8                           
-vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               
+local options = {
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = false,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  termguicolors = true,
+  timeoutlen = 1000,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = false,
+  numberwidth = 4,
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  guifont = "monospace:h17",
+}
 
 vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
